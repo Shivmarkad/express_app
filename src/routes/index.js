61ec55ae8092ce5684e642c4489bbb07ff72fe85
Shivganesh Digambar Markad <85576625+Shivmarkad@users.noreply.github.com>
2023-10-25 13:28:14 +0530

    Update index.js

--- src/routes/index.js
+++ src/routes/index.js
@@ -14,6 +14,6 @@ const routes = () =>{
 
 function getdetails(str,res){
     console.log('hi there am in jenkins console')
-    return 'hello there now it is getting update jenkin server also'
+    return 'hello there now it is getting update jenkin server also checking again'
 }
 export default routes;
